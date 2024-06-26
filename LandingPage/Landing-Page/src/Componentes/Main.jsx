4389a05d481c1b8f38ed0719e2b0d3504062c31c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-26 08:47:54 -0300

    Landing Page - TCC

--- LandingPage/Landing-Page/src/Componentes/Main.jsx
+++ LandingPage/Landing-Page/src/Componentes/Main.jsx
@@ -6,7 +6,7 @@ function Main() {
           <h2 className="text-white mb-8 text-7xl text-wrap px-44 pt-20 font-bold">Uma plataforma que automatiza o processo de Check-In de Hoteis</h2>
           <p className="text-white  text-wrap w-2/4 text-xl">Entregamos um serviço completo de automatização, desde formulário até
              uma plataforma de controle de hóspedes com <em className="font-bold">reconhecimento facial</em>.</p>
-            <a className="absolute top-[500px]" href="">
+            <a className="absolute top-[500px]" href="https://www.figma.com/proto/uKTUi64kzm81YUOfJaj5KE/Check-All---TCC?node-id=52-4&t=m7V82aq0G0jcsHpK-0&scaling=min-zoom&content-scaling=fixed&page-id=2%3A2&starting-point-node-id=52%3A4">
             <img src="Frame1.png" alt="Tela de Login" className="mx-auto w-3/4 h-auto" />
           </a> 
         </div>
