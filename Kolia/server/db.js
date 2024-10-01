38f7a35e108da82c805058ec18e2097a44aea0ef
Thomas <thomas@qpix.com.br>
2024-09-30 23:44:28 -0300

    outra tentativa falha

--- Kolia/server/db.js
+++ Kolia/server/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export default async function conectaNaDb() {
-    mongoose.connect("mongodb+srv://admin:admin@koliadatabase.1dnjc.mongodb.net/?retryWrites=true&w=majority&appName=KoliaDataBase")
+    mongoose.connect("mongodb+srv://admin:admin@koliadatabase.1dnjc.mongodb.net/Kolia?retryWrites=true&w=majority&appName=KoliaDataBase")
     return mongoose.connection;
 }
